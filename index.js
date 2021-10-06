b03b081af7955c7875b312da4da3a8e4e5c26711
Arely Rivera <emmarely.rivera@gmail.com>
2021-10-06 00:00:04 -0500

    Ejercicios-webpack-babel

---
+++ index.js
@@ -0,0 +1,24 @@
+
+import 'fslightbox';
+/* import {suma, saludo} from './operaciones';
+
+document.write(`<h1'${suma(1,2)}</h1>`)
+
+console.log(suma(1,2));
+console.log(saludo);
+
+
+
+
+ */
+/* import _ from 'lodash';
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component()); */
---
+++ operaciones.js
@@ -0,0 +1,14 @@
+export const suma = (num1, num2) => {
+    return num1 + num2;
+
+}
+const resta = (num1, num2) => {
+    return num1 - num2;
+
+}
+const multiplicacion = (num1, num2) => {
+    return num1 * num2;
+
+}
+
+export const saludo = 'Hola, soy Emma Arely, saludos desde un módulo';
